1eeb4f55c09a2ef16aedd95a5527f20bc3069147
DumDereDum <46279571+DumDereDum@users.noreply.github.com>
2019-12-03 15:43:24 +0300

    Add files via upload

--- lab2/lab2.R
+++ lab2/lab2.R
@@ -39,7 +39,7 @@ confIntVarwMean <- function(arr, G){
 # Confidence interval for variance without mean
 # доверительный интервал для математического ожидания при неизвестной дисперсии
 # G - Gamma  
-  N = length(arr)
+  N  = length(arr)
   a1 = (1 - G)/2
   a2 = (1 + G)/2
   
@@ -50,6 +50,62 @@ confIntVarwMean <- function(arr, G){
   R = sqrt(N-1)*S / sqrt(qchisq(a2, N-1)); R # Right
   #res = c(L, R); res
   return(c(L, R))
+}
+
+
+intLen <- function(int){
+#Длинна интервала
+  len <- abs(max(int) - min(int))
+  return (len)
+}
+
+
+plot1 <- function(arr1, arr2, S){
+  confProps <- seq(from=0.50, to=1, by=0.02); confProps
+  lengths1 <- c(1:length(confProps))
+  lengths2 <- c(1:length(confProps))
+  for(i in 1: length(confProps)){
+    lengths1[i] <- intLen(confIntMeanVar(arr1, S, confProps[i]))
+    lengths2[i] <- intLen(confIntMeanVar(arr2, S, confProps[i]))
+  }; lengths
+  plot(confProps, lengths2,  col = "red", type = "l", 
+       xlab = "Дов. вер-ть", 
+       ylab = "дов. интервал",
+       main = "Доверительный интервал для оценки математического ожидания при известной дисперсии")
+  lines(confProps, lengths1,  col = "green")
+}
+
+
+plot2 <- function(arr1, arr2, S){
+  confProps <- seq(from=0.50, to=1, by=0.02); confProps
+  lengths1 <- c(1:length(confProps))
+  lengths2 <- c(1:length(confProps))
+  for(i in 1: length(confProps)){
+    lengths1[i] <- intLen(confIntMeanwVar(arr1, confProps[i]))
+    lengths2[i] <- intLen(confIntMeanwVar(arr2, confProps[i]))
+  }; lengths
+  
+  plot(confProps, lengths2,  col = "red", type = "l", 
+       xlab = "Дов. вер-ть", 
+       ylab = "дов. интервал",
+       main = "Доверительный интервал для оценки математического ожидания при неизвестной дисперсии")
+  lines(confProps, lengths1,  col = "green")
+}
+
+
+plot3 <- function(arr1, arr2, S){
+  confProps <- seq(from=0.50, to=1, by=0.02); confProps
+  lengths1 <- c(1:length(confProps))
+  lengths2 <- c(1:length(confProps))
+  for(i in 1: length(confProps)){
+    lengths1[i] <- intLen(confIntVarwMean(arr1, confProps[i]))
+    lengths2[i] <- intLen(confIntVarwMean(arr2, confProps[i]))
+  }; lengths
+  plot(confProps, lengths2,  col = "red", type = "l", 
+       xlab = "Дов. вер-ть", 
+       ylab = "дов. интервал",
+       main = "Доверительный интервал для оценки дисперсии при неизвестной математическом ожидании")
+  lines(confProps, lengths1,  col = "green")
 }
 
 
@@ -73,3 +129,9 @@ interval2 <- confIntMeanwVar(sample2, G); interval2
 interval1 <- confIntVarwMean(sample1, G); interval1
 interval2 <- confIntVarwMean(sample2, G); interval2
 
+
+dev.off()
+par(mfcol=c(3, 1))
+plot1(sample1,sample2, S)
+plot2(sample1,sample2, S)
+plot3(sample1,sample2, S)
