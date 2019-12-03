61f81d49b45efd31c715c67662f5654cf3d27aa6
DumDereDum <46279571+DumDereDum@users.noreply.github.com>
2019-12-03 12:47:24 +0300

    Add files via upload

--- lab2/lab2.R
+++ lab2/lab2.R
@@ -3,37 +3,51 @@
 # Sigma = 2.5
 
 
-Laplas <- function(t){
-  return(round( ((pnorm(t)-pnorm(-t))/2), 4))
-}
-#Laplas(0.5)
-
-find_t <- function(F){
-  t = 0
-  i = 0
-  while (abs(Laplas(t)-F)>0.0001){
-    if (Laplas(t)-F > 0){
-      t = t - 5/2^i
-    }
-    else {  
-      t = t + 5/2^i
-    }
-  i = i +1
-  }
-  return(round(t, 2))
-}
-#find_t(0.1914)
-
-convInMeanKnownVar <- function(arr, S, G){
+confIntMeanVar <- function(arr, S, G){
+# Confidence interval for mean variance
 # доверительный интервал для математического ожидания при известной дисперсии
 # S - Sigma (variance)
 # G - Gamma  
   N = length(arr)
-  t = find_t(G/2); t
+  d = qnorm(G) * S /sqrt(N); d # Delta
+    
+  L = mean(sample1) - d; L # Left 
+  R = mean(sample1) + d; R # Right
+  #res = c(L, R); res
+  return(c(L, R))
+}
+
+
+confIntMeanwVar <- function(arr, G){
+# Confidence interval for mean without variance
+# доверительный интервал для математического ожидания при неизвестной дисперсии
+# G - Gamma  
+  N = length(arr)
+  t = qt(G, N-1); t
+  D = var(arr)          # Sample Variance
+  S = sqrt((N/(N-1))*D) # Corrected Sigma
   d = t * S /sqrt(N); d # Delta
   
   L = mean(sample1) - d; L # Left 
   R = mean(sample1) + d; R # Right
+  #res = c(L, R); res
+  return(c(L, R))
+}
+
+
+confIntVarwMean <- function(arr, G){
+# Confidence interval for variance without mean
+# доверительный интервал для математического ожидания при неизвестной дисперсии
+# G - Gamma  
+  N = length(arr)
+  a1 = (1 - G)/2
+  a2 = (1 + G)/2
+  
+  D = var(arr)          # Sample Variance
+  S = sqrt((N/(N-1))*D) # Corrected Sigma
+  
+  L = sqrt(N-1)*S / sqrt(qchisq(a1, N-1)); L # Left 
+  R = sqrt(N-1)*S / sqrt(qchisq(a2, N-1)); R # Right
   #res = c(L, R); res
   return(c(L, R))
 }
@@ -49,20 +63,13 @@ N2 = 200
 sample1 <- rnorm(N1, mean = M, sd = S); sample1
 sample2 <- rnorm(N2, mean = M, sd = S); sample2
 
-interval1 <- convInMeanKnownVar(sample1, S, G); interval1
-interval2 <- convInMeanKnownVar(sample2, S, G); interval2
 
-G = 0.95
-k = 10 - 1
+interval1 <- confIntMeanVar(sample1, S, G); interval1
+interval2 <- confIntMeanVar(sample2, S, G); interval2
 
-dt(0.95, 9, log = FALSE)
-pt(0.95, 9, lower.tail = TRUE, log.p = FALSE)
-qt(0.95, 9, lower.tail = TRUE, log.p = FALSE)
-rt(1, 9,0.95)
+interval1 <- confIntMeanwVar(sample1, G); interval1
+interval2 <- confIntMeanwVar(sample2, G); interval2
 
-t.test(conf.level = 9)
-help(t.test)
+interval1 <- confIntVarwMean(sample1, G); interval1
+interval2 <- confIntVarwMean(sample2, G); interval2
 
-dchisq(0.95, 9, ncp=0)
-pchisq(0.95, 9)
-pchisq(0.95, 9, ncp=0)
